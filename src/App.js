7e3a46dadfaa6d84f506b260d6263ae41de61daa
Igor <i.makhonkov@gmail.com>
2018-10-30 16:48:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Car from './car/car'
 
 class App extends Component {
-  render() {
+    state = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Audi', year: 2016},
+            {name: 'Mazda', year: 2010}
+        ],
+        pageTitle: 'React components',
+        showCars: false
+    }
+    toggleCarsHacndler = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        })
+    }
+
+    onChangeName(name, index) {
+        const car = this.state.cars[index]
+        car.name = name
+        const cars = [...this.state.cars]
+        cars[index] = car
+        this.setState({cars})
+    }
+    deleteHandler(index) {    
+        const cars = this.state.cars.concat()
+        cars.splice(index, 1)
+
+        this.setState({cars})
+    }
+    render() {
+        const divStyle = {
+        textAlign: 'center'
+    }
+    let cars = null
+
+    if (this.state.showCars) {
+        cars = this.state.cars.map((car, index) => {
+            return (
+                <Car 
+                    key={index}
+                    name={car.name}
+                    year={car.year}
+                    onDelete={this.deleteHandler.bind(this, index)}
+                    onChangeName = {event => this.onChangeName(event.target.value, index)
+                    }
+                />
+            )
+        }) 
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div style={divStyle}>
+            <h1>{this.state.pageTitle}</h1>
+            <button onClick={this.toggleCarsHacndler}>Toggle cars</button>
+            <div style={{
+                width: 400,
+                margin: 'auto',
+                paddingTop: '20px'
+            }}>
+                { cars }   
+            </div>
+        </div>
     );
-  }
+    }
 }
 
 export default App;
